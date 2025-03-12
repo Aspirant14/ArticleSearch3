4d4fe1679e94719f2320587686adfbd8f06b9151
Henry Torres Hernandez <torres.henry14@gmail.com>
2025-03-11 20:40:41 -0400

    Fixed R.id.bottom_navigation null pointer exception.

--- app/src/main/java/com/codepath/articlesearch/MainActivity.kt
+++ app/src/main/java/com/codepath/articlesearch/MainActivity.kt
@@ -3,12 +3,15 @@ package com.codepath.articlesearch
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.codepath.articlesearch.databinding.ActivityMainBinding
 import com.codepath.asynchttpclient.AsyncHttpClient
 import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import kotlinx.serialization.json.Json
 import okhttp3.Headers
 import org.json.JSONException
@@ -34,8 +37,31 @@ class MainActivity : AppCompatActivity() {
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
+        // It is important to place the R.id references after this line. Otherwise, they will not be
+        // recognized.
         setContentView(view)
+        val bottomNavigationView: BottomNavigationView = findViewById(R.id.bottom_navigation)
 
+        // Set default selection
+        bottomNavigationView.selectedItemId = R.id.BooksTab// handle navigation selection
+
+        val fragmentManager: FragmentManager = supportFragmentManager
+
+        // define your fragments here
+        val fragment1: Fragment = Fragment()
+        val fragment2: Fragment = Fragment()
+
+        bottomNavigationView.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.BooksTab ->
+                    // do something here
+                    true
+                R.id.ArticlesTab ->
+                    // do something here
+                    true
+                else -> true
+            }
+        }
         articlesRecyclerView = findViewById(R.id.articles)
         val articleAdapter = ArticleAdapter(this, articles)
         articlesRecyclerView.adapter = articleAdapter
@@ -70,8 +96,7 @@ class MainActivity : AppCompatActivity() {
                     Log.e(TAG, "Exception: $e")
                 }
             }
-
         })
-
     }
 }
+// java.lang.NullPointerException: findViewById(R.id.bottom_navigation) must not be null
